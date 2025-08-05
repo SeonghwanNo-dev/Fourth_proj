f948ab42e2490fb110324c5d0f957026d2f6f383
nanamamgaga <seonghwangood01@gmail.com>
2025-08-05 23:38:19 +0900

    Initial project setup

---
+++ README.md
@@ -0,0 +1,16 @@
+# ORM 학습용 프로젝트
+
+### 1. 프로젝트 소개
+본 프로젝트는 "자바 ORM 표준 JPA 프로그래밍, 김영한 지음" 교재를 기반으로 개발한 웹 애플리케이션 입니다.<br>
+프로젝트의 목표는 객체와 데이터베이스 사이 연결 기술인 JPA의 학습에 있습니다.
+
+
+### 2. 기술 스택
+-   **Backend:** Java
+
+-   **Framework:** Spring Boot 3, Spring Data JPA
+
+-   **Database:** H2
+
+-   **Build Tool:** Maven
+
---
+++ src/main/java/com/github/SeonghwanNo_dev/fourth_proj/FourthProjApplication.java
@@ -0,0 +1,13 @@
+package com.github.SeonghwanNo_dev.fourth_proj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FourthProjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FourthProjApplication.class, args);
+	}
+
+}
